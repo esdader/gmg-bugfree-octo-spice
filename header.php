6a8f2937ce9c3cf01f297b2ada9d7d4eb577f16b
Tristan Ader <tristanader@gmail.com>
2014-09-15 11:23:33 -0500

    stubbed out header and nav

--- header.php
+++ header.php
@@ -17,3 +17,15 @@
 </head>
 <body>
 <div class="l-container">
+<header class="main-header l-main-header clearfix">
+	<nav class="main-nav l-main-nav">
+		<ul class="l-horizontal-list">
+			<li class="l-mission"><a href="<?php echo get_page_link(8); ?>">Mission</a></li>
+			<li class="l-clients"><a href="<?php echo get_page_link(10); ?>">Clients</a></li>
+			<li class="l-expertise"><a href="<?php echo get_page_link(12); ?>">Expertise</a></li>
+			<li class="l-team"><a href="<?php echo get_page_link(14); ?>">Team</a></li>
+			<li class="l-contact"><a href="<?php echo get_page_link(16); ?>">Contact</a></li>
+		</ul>
+	</nav>
+	<a href="<?php echo home_url(); ?>" class="l-logo-con"><img src="<?php bloginfo('template_directory'); ?>/img/logo.svg" onerror="this.src='<?php bloginfo('template_directory'); ?>/img/logo.png'" alt="<?php bloginfo('name'); ?>"></a>
+</header>
