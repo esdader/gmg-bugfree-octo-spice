a4e514798c834ab9d17a3e22f8d3eaed25021b85
Tristan Ader <tristanader@gmail.com>
2014-09-16 16:22:38 -0500

    adding tag-line image to homepage

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php get_header(); ?>
 
-<p>Hello world! This is HTML5 Boilerplate.</p>
 
 <?php get_footer(); ?>
---
+++ page-31.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Homepage
+ */
+
+
+get_header(); 
+
+?>
+
+<div class="l-welcome-panel">
+	<img src="<?php bloginfo('template_directory'); ?>/img/tag-line.svg" onerror="this.src='<?php bloginfo('template_directory'); ?>/img/tag-line.png'" alt="GMG The Unrelenting Champions of Brand &trade;">
+</div>
+
+<?php get_footer(); ?>
