40bd6b6eb0204063067761037f7a0c6dc2dc3d45
Tristan Ader <tristanader@gmail.com>
2014-09-17 01:53:21 -0500

    added clients page

---
+++ page-10.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Clients page
+ */
+
+
+get_header(); 
+?>
+
+<div class="l-welcome-panel">
+	<img src="<?php bloginfo('template_directory'); ?>/img/clients-grid.jpg" alt="List of past client logos">
+</div>
+
+<?php get_footer(); ?>
