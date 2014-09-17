0cd6d2401c6f34a2be8f8ee4a33fdacb43d66666
Tristan Ader <tristanader@gmail.com>
2014-09-17 03:10:15 -0500

    first pass at team archive page

--- archive-team-members.php
+++ archive-team-members.php
@@ -7,5 +7,27 @@
 get_header();
 
 ?>
-
+<div class="l-team-container">
+	<div class="l-team-holder">
+		<ul class="l-team-list team-list">
+			<?php 
+				$args = array(
+							'post_type' => 'team-members',
+							'order_by'  => 'menu-order',
+							'order'     => 'ASC'
+						);
+				$team = new WP_Query($args);
+			
+			 if ( $team->have_posts() ) : while ( $team->have_posts() ) : $team->the_post(); ?>
+				<li>
+					<a href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>
+					</a>
+				</li>
+			<?php endwhile; ?>
+			<?php else: ?>
+			<?php endif; ?>
+		</ul>
+	</div>
+</div>
 <?php get_footer(); ?>
